8c82916a3412ebb69021cbb11a626f8a316a002b
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2020-11-18 17:53:21 +0100

    #menu_bar Implementacija akceleratora i mnemonika

--- projekat/src/gui/MenuBar.java
+++ projekat/src/gui/MenuBar.java
@@ -1,9 +1,13 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
 
 
 public class MenuBar extends JMenuBar {
@@ -19,22 +23,36 @@ public class MenuBar extends JMenuBar {
 		JMenu edit = new JMenu("Edit");
 		JMenu help = new JMenu("Help");
 
-		JMenuItem m_new = new JMenuItem("New", new ImageIcon("images/new.png"));		
-		JMenuItem m_close = new JMenuItem("Close", new ImageIcon("images/close.png"));
+		
+		file.setMnemonic(KeyEvent.VK_F);
+		edit.setMnemonic(KeyEvent.VK_E);
+		help.setMnemonic(KeyEvent.VK_H);
+		
+		JMenuItem m_new = new JMenuItem(" New", new ImageIcon("images/new.png"));
+		m_new.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK));
+		
+		JMenuItem m_close = new JMenuItem(" Close", new ImageIcon("images/close.png"));
+		m_close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK));
 		
 		file.add(m_new);
 		file.addSeparator();
 		file.add(m_close);
 		
-		JMenuItem m_edit = new JMenuItem("Edit", new ImageIcon("images/edit.png"));
-		JMenuItem m_delete = new JMenuItem("Delete", new ImageIcon("images/delete.png"));
+		JMenuItem m_edit = new JMenuItem(" Edit", new ImageIcon("images/edit.png"));
+		m_edit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.ALT_MASK));
+		
+		JMenuItem m_delete = new JMenuItem(" Delete", new ImageIcon("images/delete.png"));
+		m_delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.ALT_MASK));
 		
 		edit.add(m_edit);
 		edit.addSeparator();
 		edit.add(m_delete);
 		
-		JMenuItem m_help= new JMenuItem("Help", new ImageIcon("images/help.png"));
-		JMenuItem m_about = new JMenuItem("About", new ImageIcon("images/about.png"));
+		JMenuItem m_help= new JMenuItem(" Help", new ImageIcon("images/help.png"));
+		m_help.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.ALT_MASK));
+		
+		JMenuItem m_about = new JMenuItem(" About", new ImageIcon("images/about.png"));
+		m_about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK));
 		
 		help.add(m_help);
 		help.addSeparator();
