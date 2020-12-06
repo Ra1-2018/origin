41965bcd4d1c40f2b2ce648852d3fcc114014bb6
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2020-12-06 16:35:48 +0100

    #glavni_prozor uredjivanje

--- projekat/src/gui/Frame.java
+++ projekat/src/gui/Frame.java
@@ -5,8 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-
-
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -35,41 +35,7 @@ public class Frame extends JFrame {
 		setTitle("Studentska služba");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
-		Toolbar toolbar = new Toolbar();
-		add(toolbar, BorderLayout.NORTH);
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.GRAY);
 		this.add(panel);
@@ -77,6 +43,32 @@ public class Frame extends JFrame {
 		todolabel.setForeground(Color.WHITE);
 		panel.add(todolabel);
 		
+		Toolbar toolbar = new Toolbar();
+		add(toolbar, BorderLayout.NORTH);
+		
+		JPanel statusBar = new JPanel();
+		add(statusBar, BorderLayout.SOUTH);
+		
+		BoxLayout box = new BoxLayout(statusBar, BoxLayout.X_AXIS);
+		statusBar.setLayout(box);
+		statusBar.setBackground(Color.WHITE);
+		statusBar.setPreferredSize(new Dimension(100, 30));
+		
+		JLabel statusLabel = new JLabel("Studentska služba");
+		statusBar.add(Box.createHorizontalStrut(15));
+		statusBar.add(statusLabel);
+		statusBar.add(Box.createHorizontalGlue());
 
+		
+		JLabel trenutni_datum = new JLabel();
+		JLabel time = new JLabel();
+		
+		CurrentTime sat = new CurrentTime(time, trenutni_datum);
+		sat.start();
+		
+		statusBar.add(time);
+		statusBar.add(Box.createHorizontalStrut(5)); 
+		statusBar.add(trenutni_datum);
+		statusBar.add(Box.createHorizontalStrut(15)); 
 	}
 }
