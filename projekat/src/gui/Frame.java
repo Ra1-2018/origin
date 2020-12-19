aead0367cb51419c3758b54c1297812a21387502
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2020-12-19 16:30:24 +0100

    #glavni_prozor uvodjenje singleton pattern-a i generalno preuredjivanje klase

--- projekat/src/gui/Frame.java
+++ projekat/src/gui/Frame.java
@@ -9,66 +9,92 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+
 import javax.swing.JLabel;
-
-
-
 
 public class Frame extends JFrame {
 	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 4743270606172960944L;
+	private static final long serialVersionUID = -3821033433179171038L;
 
-	public Frame() {
+	private Toolbar toolbar;
+	private MenuBar menu;
+	private JPanel statusBar;
+	private JLabel statusLabel;
+	private JLabel trenutni_datum;
+	private JLabel time;
+	private CurrentTime sat;
+	
+	private static Frame instance = null;
+
+	
+	
+	private Frame() {
+		this.initPosition();
+		this.createStatusbar();
 		
-		MenuBar menu = new MenuBar();
-		this.setJMenuBar(menu);
+		this.menu = new MenuBar();
+		this.setJMenuBar(this.menu);
 		
+		this.toolbar = new Toolbar();
+		this.add(this.toolbar, BorderLayout.NORTH);
+	
+		//JPanel panel = new JPanel();
+		//panel.setBackground(Color.GRAY);
+		//this.add(panel);
+		//JLabel todolabel = new JLabel("TODO: prikaz entiteta sistema", JLabel.CENTER);
+		//todolabel.setForeground(Color.WHITE);
+		//panel.add(todolabel);
+	}
+		
+	public static Frame getInstance() {
+		if (instance == null) {
+			instance = new Frame();
+		}
+		return instance;
+	}
+	
+	private void initPosition() {
 		Toolkit kit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = kit.getScreenSize();
 		int screenHeight = screenSize.height;
 		int screenWidth = screenSize.width;
 		
-		setSize(screenWidth * 3 / 4, screenHeight * 3 / 4);
-		setTitle("Studentska služba");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
+		this.setSize(screenWidth * 3 / 4, screenHeight * 3 / 4);
+		this.setTitle("Studentska služba");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
 	
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.GRAY);
-		this.add(panel);
-		JLabel todolabel = new JLabel("TODO: prikaz entiteta sistema", JLabel.CENTER);
-		todolabel.setForeground(Color.WHITE);
-		panel.add(todolabel);
-		
-		Toolbar toolbar = new Toolbar();
-		add(toolbar, BorderLayout.NORTH);
-		
-		JPanel statusBar = new JPanel();
-		add(statusBar, BorderLayout.SOUTH);
+	
+	private void createStatusbar() {
+		this.statusBar = new JPanel();
+		this.add(this.statusBar, BorderLayout.SOUTH);
 		
 		BoxLayout box = new BoxLayout(statusBar, BoxLayout.X_AXIS);
-		statusBar.setLayout(box);
-		statusBar.setBackground(Color.WHITE);
-		statusBar.setPreferredSize(new Dimension(100, 30));
+		this.statusBar.setLayout(box);
+		this.statusBar.setBackground(Color.WHITE);
+		this.statusBar.setPreferredSize(new Dimension(100, 30));
 		
-		JLabel statusLabel = new JLabel("Studentska služba");
-		statusBar.add(Box.createHorizontalStrut(15));
-		statusBar.add(statusLabel);
-		statusBar.add(Box.createHorizontalGlue());
+		this.statusLabel = new JLabel("Studentska služba");
+		this.statusBar.add(Box.createHorizontalStrut(15));
+		this.statusBar.add(statusLabel);
+		this.statusBar.add(Box.createHorizontalGlue());
 
 		
-		JLabel trenutni_datum = new JLabel();
-		JLabel time = new JLabel();
+		this.trenutni_datum = new JLabel();
+		this.time = new JLabel();
 		
-		CurrentTime sat = new CurrentTime(time, trenutni_datum);
-		sat.start();
+		this.sat = new CurrentTime(this.time, this.trenutni_datum);
+		this.sat.start();
 		
-		statusBar.add(time);
-		statusBar.add(Box.createHorizontalStrut(5)); 
-		statusBar.add(trenutni_datum);
-		statusBar.add(Box.createHorizontalStrut(15)); 
+		this.statusBar.add(this.time);
+		this.statusBar.add(Box.createHorizontalStrut(5)); 
+		this.statusBar.add(this.trenutni_datum);
+		this.statusBar.add(Box.createHorizontalStrut(15));
 	}
 }
