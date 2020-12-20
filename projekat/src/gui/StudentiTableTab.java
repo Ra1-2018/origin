d887fadbe55fee81b51c427e4626989de6530935
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2020-12-20 18:12:07 +0100

    #prikaz_studenata pravljenje zasebne klase za tab sa studentima

---
+++ projekat/src/gui/StudentiTableTab.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.JPanel;
+
+public class StudentiTableTab extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2992467426807680946L;
+
+}
