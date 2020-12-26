2675057c31e32dfb330a694ef36f0b806405c5a1
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2020-12-26 19:48:09 +0100

    #menu_bar implementacija izlaska iz aplikacije

--- projekat/src/gui/MenuBar.java
+++ projekat/src/gui/MenuBar.java
@@ -3,11 +3,15 @@ package gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+import javax.swing.WindowConstants;
 
 
 
@@ -59,6 +63,9 @@ public class MenuBar extends JMenuBar {
 		JMenuItem m_close = new JMenuItem(" Close", new ImageIcon("images/close.png"));
 		m_close.setMnemonic(KeyEvent.VK_C);
 		m_close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK));
+		m_close.addActionListener((event) -> System.exit(0));
+		
+	
 		
 		file.add(m_new);
 		file.addSeparator();
