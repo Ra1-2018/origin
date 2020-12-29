4225b0b81a786b563bab59e1a40801aed940f6ad
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2020-12-29 17:05:40 +0100

    #prikaz_predmeta klasa PredmetiController

---
+++ projekat/src/controller/PredmetiController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.util.List;
+
+import gui.Frame;
+import model.BazaPredmeta;
+import model.Predmet;
+
+public class PredmetiController {
+
+	private static PredmetiController instance = null;
+	
+	public static PredmetiController getrInstance() {
+		if (instance == null) {
+			instance = new PredmetiController();
+		}
+		return instance;
+	}
+	
+	private PredmetiController() {}
+	
+	public List<Predmet> getPredmeti() {
+		return BazaPredmeta.getInstance().getPredmeti();
+	}
+	
+	public void dodajPredmet(Predmet predmet) {
+		BazaPredmeta.getInstance().dodajPredmet(predmet);
+		Frame.getInstance().getPredTab().azurirajPrikaz("DODAT", -1);
+	}
+}
