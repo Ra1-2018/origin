6a4aedeedc68d4869a987f41fb7e30dfde06dcbd
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2021-01-02 17:54:10 +0100

    #prikaz_profesora tabela bolje izgleda

--- projekat/src/gui/ProfesoriTableTab.java
+++ projekat/src/gui/ProfesoriTableTab.java
@@ -18,14 +18,28 @@ public class ProfesoriTableTab extends JPanel {
 	private JScrollPane scrollPane;
 	
 	public ProfesoriTableTab(String text) {
-		
+			this.setLayout(new BorderLayout());
 			this.tableName = text;
 			tabelaProfesora = new ProfesoriJTable();
-			
 			this.scrollPane = new JScrollPane(tabelaProfesora);
-			scrollPane.setPreferredSize(new Dimension(1000, 700));
-			add(scrollPane, BorderLayout.CENTER);
 
+			JPanel panNorth = new JPanel();
+			panNorth.setPreferredSize(new Dimension(1000, 30));
+			this.add(panNorth, BorderLayout.NORTH);
+			
+			JPanel panSouth = new JPanel();
+			panSouth.setPreferredSize(new Dimension(1000, 30));
+			this.add(panSouth, BorderLayout.SOUTH);
+			
+			JPanel panWest = new JPanel();
+			panWest.setPreferredSize(new Dimension(80, 1000));
+			this.add(panWest, BorderLayout.WEST);
+			
+			JPanel panEast = new JPanel();
+			panEast.setPreferredSize(new Dimension(80, 1000));
+			this.add(panEast, BorderLayout.EAST);
+			
+			add(scrollPane, BorderLayout.CENTER);
 	}
 
 	public void saveTableContent() {
