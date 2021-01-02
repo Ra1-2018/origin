71f563a6c9ecd99fe252fcce2866e35a2a0f2f6b
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2021-01-02 17:50:40 +0100

    #prikaz_predmeta tabela bolje izgleda

--- projekat/src/gui/PredmetiTableTab.java
+++ projekat/src/gui/PredmetiTableTab.java
@@ -18,12 +18,28 @@ public class PredmetiTableTab extends JPanel{
 	private JScrollPane scrollPane;
 	
 	public PredmetiTableTab(String text) {
+		this.setLayout(new BorderLayout());
 		this.tableName = text;
 		this.tabelaPredmeta = new PredmetiJTable();
 		this.scrollPane = new JScrollPane(tabelaPredmeta);
 		
-		scrollPane.setPreferredSize(new Dimension(1000, 700));
-		add(scrollPane, BorderLayout.CENTER);
+		JPanel panNorth = new JPanel();
+		panNorth.setPreferredSize(new Dimension(1000, 30));
+		this.add(panNorth, BorderLayout.NORTH);
+		
+		JPanel panSouth = new JPanel();
+		panSouth.setPreferredSize(new Dimension(1000, 30));
+		this.add(panSouth, BorderLayout.SOUTH);
+		
+		JPanel panWest = new JPanel();
+		panWest.setPreferredSize(new Dimension(80, 1000));
+		this.add(panWest, BorderLayout.WEST);
+		
+		JPanel panEast = new JPanel();
+		panEast.setPreferredSize(new Dimension(80, 1000));
+		this.add(panEast, BorderLayout.EAST);
+		
+		this.add(scrollPane, BorderLayout.CENTER);
 	}
 	
 	public void saveTableContent() {
