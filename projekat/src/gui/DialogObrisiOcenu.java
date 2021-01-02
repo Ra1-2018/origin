a44a49442f97e61d1253f35efab486ea109eee10
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2021-01-02 22:31:23 +0100

    #ponistavanje_ocene izmene u dijalogu za ponistavanje ocene to jest polozenog ispita studenta

--- projekat/src/gui/DialogObrisiOcenu.java
+++ projekat/src/gui/DialogObrisiOcenu.java
@@ -26,14 +26,11 @@ public class DialogObrisiOcenu extends JDialog {
 	
 	private Predmet predmet;
 	private NepolozeniPredmetiJTable nepolozeni;
-	private PolozeniIspitiJTable polozeni;
-
 
 	public DialogObrisiOcenu(PolozeniIspitiJTable polozeni, NepolozeniPredmetiJTable nepolozeni, Student student) {
 		
 		super();
 		this.nepolozeni = nepolozeni;
-		this.polozeni = polozeni;
 		
 		setSize(400, 150);
 		setModal(true);
@@ -106,9 +103,6 @@ public class DialogObrisiOcenu extends JDialog {
 		nepolozeniModel.fireTableDataChanged();
 		validate();
 		
-		AbstractTableModelPolozeniIspiti polozeniModel = (AbstractTableModelPolozeniIspiti) polozeni.getModel();
-		
-		polozeniModel.fireTableDataChanged();
 		validate();
 	}
 	
